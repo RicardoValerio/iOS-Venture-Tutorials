c6edcfb20229462432db61470173ebb03cd7610c
Ricardo Valério <jricvalerio@gmail.com>
2014-12-01 01:52:07 +0000

    learned about lifeCycle events

---
+++ MultitaskingDemo/MultitaskingDemo/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  MultitaskingDemo
+//
+//  Created by Simon Allardice on 10/12/14.
+//  Copyright (c) 2014 Simon Allardice. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // ===========================================================================
+    // método que é chamado quando a view deste controller é acabada ser renderizada
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        println("In ViewController - viewDidLoad")
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    // ===========================================================================
+    // Existem muitos mais métodos que podem ser implementados que gerem o lifeCycle
+    // da aplicação
+    // por exemplo
+    // viewDidDisapper()
+    // viewDidUnload()
+    // viewWillAppear()
+    // viewWillDisappear()
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        println("In ViewController - didReceiveMemoryWarning")
+        
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
