cf1f98f7637bcd5651c3d40fdc539cd469f1b8ad
Ricardo Valério <jricvalerio@gmail.com>
2015-02-12 01:20:45 +0000

    Basic Segues Flow - from one ViewController to another through buttons :zzz:

---
+++ SeguesFlow/SeguesFlow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SeguesFlow
+//
+//  Created by Ricardo Valério on 12/02/15.
+//  Copyright (c) 2015 Ricardo Valério. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
