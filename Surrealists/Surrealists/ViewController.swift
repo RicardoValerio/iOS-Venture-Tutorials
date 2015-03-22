9fa6aac421b59efa73e28403ec4db89c3e512fd3
Ricardo Valério <jricvalerio@gmail.com>
2015-03-22 20:34:57 +0000

    :doughnut:

--- Surrealists/Surrealists/ViewController.swift
+++ Surrealists/Surrealists/ViewController.swift
@@ -2,8 +2,8 @@
 //  ViewController.swift
 //  Surrealists
 //
-//  Created by Ricardo Valério on 12/02/15.
-//  Copyright (c) 2015 Ricardo Valério. All rights reserved.
+//  Created by James Bucanek on 9/27/14.
+//  Copyright (c) 2014 Apress. All rights reserved.
 //
 
 import UIKit
